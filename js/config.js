205bc9cf70956877ed2acbfe0dfa5bedb6c1544e
Dealu <42898812+emmy13@users.noreply.github.com>
2023-03-08 11:29:54 +0100

    Add files via upload

---
+++ js/config.js
@@ -0,0 +1,15 @@
+const config = {
+  apiKey: "AIzaSyDe7FeLxJ4b-r47xDloWTS7qhUC-v4348o",
+  authDomain: "chatbox-95f1a.firebaseapp.com",
+  databaseURL: "https://chatbox-95f1a-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "chatbox-95f1a",
+  storageBucket: "chatbox-95f1a.appspot.com",
+  messagingSenderId: "551987648491",
+  appId: "1:551987648491:web:1af3f70d5737456d712bae",
+  measurementId: "G-NDRMMRJ0JJ",
+  CometChatAppId: "234071e55e0bcff1",
+  CometChatRegion: "eu",
+  CometChatAuthKey: "6f0a7766461814b48bb51a055fd03f0081f84073",
+  CometChatAPIKey: "a0e4a4306322bbd19d464b4c29451887d28502b8",
+  CometChatWidgetId: "4f8bbdce-d2af-44f7-a27e-e796e4a048df"
+};
